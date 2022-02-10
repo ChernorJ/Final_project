1bf9b5c351511ca368fbf0f518eed6617ef47ff8
ChernorJ <86679167+ChernorJ@users.noreply.github.com>
2022-02-09 23:05:33 -0500

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,9 @@
+var Lshop1 = document.querySelectorAll('.food');
+for(var i=2;i<Lshop1.length; i++){
+Lshop1[i].style.backgroundColor = 'purple';
+Lshop1[i].style.color = 'pink';
+Lshop1[i].style.fontsize ='1.2em';
+}
+var btn1 = document.querySelector('click', function(){
+  alert('Hey addEventlistener!');
+});
